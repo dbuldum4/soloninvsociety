1e6184e298ba690db87cc46900743d105a335715
Deniz Aydin Buldum <59258696+dbuldum4@users.noreply.github.com>
2026-09-01 20:36:33 -0400

    chore: move app to repository root

--- .next/static/chunks/aaad2c5193e4f737.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/6cd2f586a0692668.js",
-  "static/chunks/turbopack-f22adb2b2874a54f.js"
-])
---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
+  {
+    files: ["next.config.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+
+export default eslintConfig;
